057f5ed42dd8b9f4701a702839da1ceb4b221b3e
Nader Fares <naderalifares16@hotmail.com>
2024-04-30 00:20:19 -0300

    feat: Main variables

--- main.c
+++ main.c
@@ -4,7 +4,10 @@ int main() {
   printf("\n#### ResistorCalc v1.0 ####\n\n");
   printf("Developed by: Nader Fares\n\n");
 
-  
+  // Váriaveis Principais
+  float voltage = 0; // (Volts)
+  float current = 0; // (Amper)
+  float resistance = 0; // (Ohm)
 
   system("PAUSE");
   return 0;
